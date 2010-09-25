6322101de569a234bd57b3d078bd92d1eae680ed
harm <harm@kabisa.nl>
2010-09-25 21:40:38 +0200

    fixed

--- app/models/kiosk.rb
+++ app/models/kiosk.rb
@@ -2,4 +2,7 @@ class Kiosk < ActiveRecord::Base
   belongs_to :user
   has_many :fxs
   has_attached_file :background, :storage => :s3, :s3_credentials => "#{RAILS_ROOT}/config/s3.yml", :path => "/:style/:filename"
+  
+  validates_presence_of :name
+  validates_attachment_size(:background, :less_than => 1.megabyte, :unless => "background_content_type.blank?")
 end
