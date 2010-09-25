c7c3bbf45d0ccf563d5e3fcbefef62dfe1106c43
harm <harm@kabisa.nl>
2010-09-25 13:10:22 +0200

    added path to s3

--- app/models/kiosk.rb
+++ app/models/kiosk.rb
@@ -1,3 +1,3 @@
 class Kiosk < ActiveRecord::Base
-  has_attached_file :background, :storage => :s3, :s3_credentials => "#{RAILS_ROOT}/config/s3.yml"
+  has_attached_file :background, :storage => :s3, :s3_credentials => "#{RAILS_ROOT}/config/s3.yml", :path => "/:style/:filename"
 end
